a6796066bb6d839f95d9eac02dea7ac6d1c23f91
Sangyog Puri <sangyogpuri@Sangyogs-MacBook-Air.local>
2024-11-17 09:23:22 +0545

    cli

---
+++ which_one.go
@@ -0,0 +1,32 @@
+package main
+import (
+"fmt"
+"os"
+"path/filepath"
+)
+
+func main(){
+	arguments := os.Args
+	if len(arguments) == 1{
+		fmt.Println("Please provide an argument!")
+		return
+	}
+	file := arguments[1]
+	path := os.Getenv("PATH")
+	pathSplit := filepath.SplitList(path)
+	for _, directory := range pathSplit {
+		fullpath := filepath.Join(directory,file)
+		fileInfo, err := os.Stat(fullpath) 
+		if err != nil{
+			continue
+		}
+		mode := fileInfo.Mode()
+		if !mode.IsRegular() {
+			continue
+		}
+		if mode&0111 != 0{ //is it executable?
+			fmt.Println(fullpath)
+			return
+		}
+	}
+}
